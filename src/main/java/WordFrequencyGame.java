eb2a806d26dc19f0d46a77073c43ee9b162f39f0
LISH5 <727904507@qq.com>
2020-07-27 15:44:40 +0800

    [WordFrequencyGame][ShaoLi] refactor: extract SPACE_PATTERN

--- src/main/java/WordFrequencyGame.java
+++ src/main/java/WordFrequencyGame.java
@@ -1,12 +1,15 @@
 import java.util.*;
 
 public class WordFrequencyGame {
+
+    public static final String SPACE_PATTERN = "\\s+";
+
     public String getResult(String inputStr) {
-        if (inputStr.split("\\s+").length == 1) {
+        if (inputStr.split(SPACE_PATTERN).length == 1) {
             return inputStr + " 1";
         } else {
             try {
-                String[] arr = inputStr.split("\\s+");
+                String[] arr = inputStr.split(SPACE_PATTERN);
                 List<Input> inputList = new ArrayList<>();
                 for (String s : arr) {
                     Input input = new Input(s, 1);
